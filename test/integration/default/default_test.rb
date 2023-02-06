f49dad600e64addcbcb4a93444ebd7d50a62371c
Earth U <iskitingbords@gmail.com>
2023-02-06 02:41:45 +0000

    More cookstyle corrections

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -7,25 +7,21 @@ vove = p['override'].key?('test') ? p['override']['test'] : {}
 v    = (vdef.merge(vnor)).merge(vove)
 
 describe command('composer --version') do
-  #its('stdout') { should match /Composer version 2\.3\.7/ }
   its('stdout') { should match /Composer version #{v['composer_version']}/ }
 end
 
 php_str = "php#{v['php_version']}-fpm"
 
-#describe service('php8.1-fpm.service') do
 describe service("#{php_str}.service") do
   it { should be_installed }
   it { should be_enabled }
   it { should be_running }
 end
 
-#describe file('/run/php/php8.1-fpm.pid') do
 describe file("/run/php/#{php_str}.pid") do
   it { should exist }
 end
 
-#describe file('/run/php/php8.1-fpm.sock') do
 describe file("/run/php/#{php_str}.sock") do
   it { should exist }
   its('type') { should eq :socket }
@@ -35,12 +31,6 @@ describe http('http://localhost') do
   its('status') { should eq 200 }
 end
 
-#%w(
-#  mysql
-#  curl
-#  zip
-#  mbstring
-#).each do |x|
 v['php_exts'].each do |x|
   describe command("php -m | grep #{x}") do
     its('exit_status') { should eq 0 }
